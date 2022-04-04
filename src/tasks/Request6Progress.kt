b59366394c8c78a12f073baf2bd9678174e10196
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2022-04-04 19:00:21 +0300

    1st task - display contributors with visual progress

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,17 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    var allUsers = emptyList<User>()
+    repos.forEachIndexed { index, repo ->
+        val contribsByRepo = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        allUsers = (allUsers + contribsByRepo).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }

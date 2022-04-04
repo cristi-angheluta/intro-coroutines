bd70340ef91d8e267b52536284f25bc87b65c5d9
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2022-04-04 20:06:41 +0300

    1st task - concurrent loading of contributors + progressive display of them (channels)

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -9,8 +9,28 @@ suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
-) {
-    coroutineScope {
-        TODO()
+) = coroutineScope {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val channel = Channel<List<User>>() // rendez-vous channel ( exchange point )
+
+    repos.forEach { repo ->
+        launch {
+            log("starting loading for ${repo.name}")
+
+            val contribsByRepo = service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }.bodyList()
+
+            channel.send(contribsByRepo)
+        }
+    }
+    var allUsers = emptyList<User>()
+    repeat(repos.size) {
+        val users = channel.receive()
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, it == repos.lastIndex)
     }
 }

fbed5d11abf8c9da4c02d259264e73a1711275df
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2022-04-04 18:23:11 +0300

    1st task - fetch contributors asynchronously

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,8 +1,27 @@
 package tasks
 
 import contributors.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val contribDeferred = repos.map { repo ->
+        val contributorsByRepo = async {
+            log("starting loading for ${repo.name}")
+
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }.bodyList()
+        }
+        contributorsByRepo
+    }
+
+    val contribs = contribDeferred.awaitAll().flatten()
+
+    contribs.aggregate()
 }
